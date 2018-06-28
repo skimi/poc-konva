a36c5dba1cdd312e753a1958ef227dd8626ed870
Remi Gebski <rgebski@lafourchette.com>
2018-06-28 14:49:58 +0200

    refactor: make multi select on by default

--- src/App.js
+++ src/App.js
@@ -19,9 +19,55 @@ class App extends Component {
   stage = null
   mainLayer = null
   transformer = null
-  selectingMultiple = false
   container = React.createRef()
   tableCount = 0
+
+  commitTransformation = () => {
+    const tables = this.selectGroup.getChildren();
+    const transform = tables.map(node => ({
+      rotation: this.selectGroup.rotation(),
+      scale: node.getAbsoluteScale(),
+      position: node.getAbsolutePosition(),
+    }));
+    this.selectGroup.removeChildren();
+    this.selectGroup.position({ x: 0, y: 0 });
+    this.selectGroup.scale({ x: 1, y: 1 });
+    this.selectGroup.rotation(0);
+    this.transformer.visible(false)
+    this.mainLayer.add(this.selectGroup);
+    tables.forEach((table, i) => {
+      this.mainLayer.add(table);
+      table.rotation(transform[i].rotation + table.rotation());
+      table.scale(transform[i].scale);
+      table.position(transform[i].position);
+      this.stage.find(`#text-${table.getAttr('id')}`)[0].moveToTop();
+      this.mainLayer.draw();
+    })
+  }
+
+  selectTable = (table, text) => {
+    const selectedTables = this.selectGroup.getChildren();
+
+    if (selectedTables.length) {
+      this.commitTransformation();
+      selectedTables.forEach((selectedTable) => {
+        this.selectGroup.add(selectedTable);
+      })
+    }
+
+    this.selectGroup.add(table);
+
+    this.selectGroup.visible(true)
+    this.selectGroup.draggable(true)
+
+    this.transformer.visible(true);
+    this.transformer.moveToTop();
+    this.transformer.forceUpdate();
+
+    text.moveToTop();
+
+    this.mainLayer.draw();
+  }
 
   componentDidMount() {
     this.stage = new Konva.Stage({
@@ -30,6 +76,7 @@ class App extends Component {
       height: this.container.current.getBoundingClientRect().height,
     });
     this.transformer = new Konva.Transformer({
+      visible: false,
       boundBoxFunc: function (oldBoundBox, newBoundBox) {
         // disallow negative scaling
         if (newBoundBox.width < 0 || newBoundBox.height < 0) {
@@ -40,14 +87,19 @@ class App extends Component {
       }
     });
     this.mainLayer = new Konva.Layer();
-    this.multipleSelectGroup = new Konva.Group();
+    this.selectGroup = new Konva.Group({
+      draggable: true,
+      visible: false,
+      id: 'selectGroup',
+    });
     this.stage.add(this.mainLayer);
     this.mainLayer.add(this.transformer);
-    this.mainLayer.add(this.multipleSelectGroup);
+    this.mainLayer.add(this.selectGroup);
     // resize the handles
     this.transformer.getChildren()
       .filter(child => RESIZERS_NAMES.includes(child.getAttr('name')))
       .forEach(resizer => resizer.setScale({ x: 2, y: 2 }));
+    this.transformer.attachTo(this.selectGroup);
     const rotator = new Konva.Circle({
       radius: 10,
       fill: 'white',
@@ -64,8 +116,8 @@ class App extends Component {
 
     const updateText = () => {
       const node = this.transformer.getNode();
-      const tables = node === this.multipleSelectGroup ?
-        this.multipleSelectGroup.getChildren() : [node]
+      const tables = node === this.selectGroup ?
+        this.selectGroup.getChildren() : [node]
 
       tables.forEach((table) => {
         const text = this.stage.find(`#text-${table.getAttr('id')}`)[0];
@@ -77,41 +129,13 @@ class App extends Component {
     };
 
     this.transformer.on('transform', updateText);
-    this.multipleSelectGroup.on('dragmove', updateText);
-
-    const handleEndMultipleSelect = () => {
-      this.selectingMultiple = false;
-      const nodes = this.multipleSelectGroup.getChildren();
-      const transform = nodes.map(node => ({
-        rotation: this.multipleSelectGroup.rotation(),
-        scale: node.getAbsoluteScale(),
-        position: node.getAbsolutePosition(),
-      }));
-      this.multipleSelectGroup.removeChildren();
-      this.multipleSelectGroup.destroy();
-      this.multipleSelectGroup = new Konva.Group();
-      this.mainLayer.add(this.multipleSelectGroup);
-      nodes.forEach((node, i) => {
-        this.mainLayer.add(node);
-        node.rotation(transform[i].rotation + node.rotation());
-        node.scale(transform[i].scale);
-        node.position(transform[i].position);
-        this.stage.find(`#text-${node.getAttr('id')}`)[0].moveToTop();
-        this.mainLayer.draw();
-      })
-    }
+    this.selectGroup.on('dragmove', updateText);
 
     const handleClickStage = (e) => {
       if (e.target === this.stage) {
-        const node = this.transformer.getNode();
-        this.stage.find('.table').forEach(table => table.draggable(false));
-        this.multipleSelectGroup.draggable(false);
-
-        if (node === this.multipleSelectGroup) {
-          handleEndMultipleSelect();
-        }
-
-        this.transformer.detach();
+        this.commitTransformation();
+        this.selectGroup.draggable(false);
+        this.transformer.visible(false);
         this.mainLayer.draw();
       }
     };
@@ -121,8 +145,8 @@ class App extends Component {
 
     const handleRotation = (e) => {
       const node = this.transformer.getNode();
-      const tables = node === this.multipleSelectGroup ?
-        this.multipleSelectGroup.getChildren() : [node];
+      const tables = node === this.selectGroup ?
+        this.selectGroup.getChildren() : [node];
       const rotatorPos = rotator.position();
       const pointerPos = {
         left: e.evt.clientX !== undefined ? e.evt.clientX : e.evt.touches[0].clientX,
@@ -149,8 +173,8 @@ class App extends Component {
         top: e.evt.clientX !== undefined ? e.evt.clientY : e.evt.touches[0].clientY
       };
       const node = this.transformer.getNode();
-      const tables = node === this.multipleSelectGroup ?
-        this.multipleSelectGroup.getChildren() : [node];
+      const tables = node === this.selectGroup ?
+        this.selectGroup.getChildren() : [node];
       tablesOriginalRotations = tables.map(table => table.rotation());
       this.stage.on('mousemove', handleRotation);
       this.stage.on('touchmove', handleRotation);
@@ -195,46 +219,18 @@ class App extends Component {
     text.offsetY(text.height() / 2);
     text.position(rect.position());
 
-    const handleResize = () => {
-      if (this.selectingMultiple) {
-        this.multipleSelectGroup.add(rect);
-        this.transformer.forceUpdate();
-      } else {
-        rect.draggable(true);
-        this.transformer.attachTo(rect);
-        rect.moveToTop();
-        this.transformer.moveToTop();
-      }
-      text.moveToTop();
-      this.mainLayer.draw();
-    };
-
     const updateText = () => {
       text.position(rect.getAbsolutePosition());
       text.moveToTop();
       this.mainLayer.draw();
     };
 
-    rect.on('click', handleResize);
-    rect.on('tap', handleResize);
+    rect.on('click', () => this.selectTable(rect, text));
+    rect.on('tap', () => this.selectTable(rect, text));
     rect.on('dragmove', updateText);
 
     this.mainLayer.add(rect);
     this.mainLayer.add(text);
-    this.mainLayer.draw();
-  }
-
-  handleSelectMultiple = () => {
-    if (this.selectingMultiple) {
-      this.transformer.detach();
-      this.multipleSelectGroup.draggable(false);
-      return;
-    }
-
-    this.selectingMultiple = true;
-    this.transformer.attachTo(this.multipleSelectGroup);
-    this.multipleSelectGroup.moveToTop();
-    this.multipleSelectGroup.draggable(true);
     this.mainLayer.draw();
   }
 
@@ -253,12 +249,6 @@ class App extends Component {
             onClick={this.handleAddTable}
           >
             Add Table
-          </button>
-          <button
-            type="button"
-            onClick={this.handleSelectMultiple}
-          >
-            Select multiple
           </button>
           <button
             type="button"
